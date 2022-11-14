cbf2d20ad5400b248e7e16ac0fcc416f78aac274
Liam Mattsson <liammattsson@hotmail.com>
2022-11-15 00:29:59 +0100

    Found some bugs
    While implementing Branch/Statement Coverage

--- src/SetWhiteBoxTests.java
+++ src/SetWhiteBoxTests.java
@@ -1,11 +1,11 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SetWhiteBoxTests {
-	
-	/* Tests for insert */
 	
 	private Set set;
 	
@@ -15,8 +15,21 @@ public class SetWhiteBoxTests {
 	}
 
 	@Test
-	void test() {
-		
+	void firstPartOfBranchAndStatementCoverage() {
+		int[] temp = new int[] {1,2,3};
+		set.insert(1);
+		set.insert(2);
+		set.insert(3);
+		assertArrayEquals(temp, set.toArray());
+	}
+	
+	@Test
+	void secondPartOfBranchAndStatementCoverage() {
+		int[] temp = new int[] {1,2,3};
+		set.insert(3);
+		set.insert(2);
+		set.insert(1);
+		assertArrayEquals(temp, set.toArray());
 	}
 
 }
