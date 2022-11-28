762faffb70ddcae9902fc77b5465df10e17b779a
ludvan <ludvan@student.chalmers.se>
2022-11-28 17:59:32 +0100

    NextIncomplete tests

--- src/WorkScheduleNextIncompleteTests.java
+++ src/WorkScheduleNextIncompleteTests.java
@@ -7,13 +7,11 @@ import org.junit.jupiter.api.Test;
 public class WorkScheduleNextIncompleteTests {
 	
 	private WorkSchedule ws1;
-	private WorkSchedule ws2;
 	private String worker;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		ws1 = new WorkSchedule(24);
-		ws2 = new WorkSchedule(24);
 		worker = "Ludvig";
 	}
 
@@ -24,24 +22,24 @@ public class WorkScheduleNextIncompleteTests {
 
 	@Test
 	void workingEmployeesLessThannEmployee() {
-		ws1.addWorkingPeriod(worker, 0, 23);
 		ws1.setRequiredNumber(2, 9, 11);
+		ws1.addWorkingPeriod(worker, 0, 23);
 		int time = ws1.nextIncomplete(0);
 		assertEquals(9, time);
 	}
 
 	@Test
 	void workingEmployeesGreatherThannEmployee() {
-		ws1.addWorkingPeriod(worker, 0, 23);
 		ws1.setRequiredNumber(1, 9, 11);
+		ws1.addWorkingPeriod(worker, 0, 23);
 		int time = ws1.nextIncomplete(0);
 		assertEquals(-1, time);
 	}
 	
 	@Test
 	void currentTimeIncomplete() {
-		ws1.addWorkingPeriod(worker, 0, 23);
 		ws1.setRequiredNumber(2, 9, 11);
+		ws1.addWorkingPeriod(worker, 0, 23);
 		int time = ws1.nextIncomplete(9);
 		assertEquals(9, time);
 	}
